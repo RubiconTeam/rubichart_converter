a6476d4c58afe2dd0a1193b358d80cc43fe8ad0f
Binpuki <30415792+Binpuki@users.noreply.github.com>
2024-11-23 04:27:07 -0600

    Merge file dialog functions

--- plugin_scene.gd
+++ plugin_scene.gd
@@ -39,10 +39,11 @@ func _ready() -> void:
 		chart_type_selection.add_item(current.get_name())
 		importers.push_back(current)
 	
-	chart_file_dialog.pressed.connect(chart_file_dialog_pressed)
-	meta_file_dialog.pressed.connect(meta_file_dialog_pressed)
-	events_file_dialog.pressed.connect(events_file_dialog_pressed)
-	output_file_dialog.pressed.connect(output_file_dialog_pressed)
+	chart_file_dialog.pressed.connect(func()->void:open_file_dialog(FileDialog.FILE_MODE_OPEN_FILE, [importers[index].get_extension()], func(path:String)->void:chart_line_edit.text=path))
+	meta_file_dialog.pressed.connect(func()->void:open_file_dialog(FileDialog.FILE_MODE_OPEN_FILE, [importers[index].get_meta_extension()], func(path:String)->void:meta_file_dialog.text=path))
+	events_file_dialog.pressed.connect(func()->void:open_file_dialog(FileDialog.FILE_MODE_OPEN_FILE, [importers[index].get_events_extension()], func(path:String)->void:events_line_edit.text=path))
+	output_file_dialog.pressed.connect(func()->void:open_file_dialog(FileDialog.FILE_MODE_OPEN_DIR, [], func(path:String)->void:output_line_edit.text=path))
+	
 	chart_type_selection.item_selected.connect(on_type_changed)
 	output_button.pressed.connect(start_convert)
 	
@@ -82,89 +83,31 @@ func print_new_line(text : String) -> void:
 	
 func clear_console() -> void:
 	console_output.text = ""
-
-func chart_file_dialog_pressed():
-	var current : Importer = importers[index]
-	if Engine.is_editor_hint():
-		var editor_file : EditorFileDialog = EditorFileDialog.new()
-		editor_file.title = "Select Chart"
-		editor_file.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILE
-		editor_file.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-		editor_file.filters = ["*" + current.get_extension()]
-		editor_file.file_selected.connect(func(path:String)->void:chart_line_edit.text=path;editor_file.queue_free())
-		editor_file.visible = true
-		add_child(editor_file)
-		return
-		
-	var file_dialog : FileDialog = FileDialog.new()
-	file_dialog.title = "Select Chart"
-	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-	file_dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-	file_dialog.filters = ["*" + current.get_extension()]
-	file_dialog.file_selected.connect(func(path:String)->void:chart_line_edit.text=path;file_dialog.queue_free())
-	file_dialog.visible = true
-	add_child(file_dialog)
 	
-func meta_file_dialog_pressed():
+func open_file_dialog(file_mode : int, extensions : PackedStringArray, on_single_chosen : Callable, on_multiple_chosen : Callable = func():):
 	var current : Importer = importers[index]
 	if Engine.is_editor_hint():
 		var editor_file : EditorFileDialog = EditorFileDialog.new()
-		editor_file.title = "Select Chart's Metadata"
-		editor_file.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILE
+		editor_file.file_mode = file_mode
 		editor_file.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-		editor_file.filters = ["*" + current.get_meta_extension()]
-		editor_file.file_selected.connect(func(path:String)->void:meta_line_edit.text=path;editor_file.queue_free())
+		editor_file.filters = extensions
+		
+		editor_file.file_selected.connect(func(path:String)->void:on_single_chosen.call(path);editor_file.queue_free())
+		editor_file.files_selected.connect(func(paths:PackedStringArray)->void:on_multiple_chosen.call(paths);editor_file.queue_free())
+		editor_file.dir_selected.connect(func(path:String)->void:on_single_chosen.call(path);editor_file.queue_free())
+		
 		editor_file.visible = true
 		add_child(editor_file)
 		return
 		
 	var file_dialog : FileDialog = FileDialog.new()
-	file_dialog.title = "Select Chart's Metadata"
-	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	file_dialog.file_mode = file_mode
 	file_dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-	file_dialog.filters = ["*" + current.get_meta_extension()]
-	file_dialog.file_selected.connect(func(path:String)->void:meta_line_edit.text=path;file_dialog.queue_free())
-	file_dialog.visible = true
-	add_child(file_dialog)
-
-func events_file_dialog_pressed():
-	var current : Importer = importers[index]
-	if Engine.is_editor_hint():
-		var editor_file : EditorFileDialog = EditorFileDialog.new()
-		editor_file.title = "Select Chart's Events"
-		editor_file.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILE
-		editor_file.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-		editor_file.filters = ["*" + current.get_events_extension()]
-		editor_file.file_selected.connect(func(path:String)->void:events_line_edit.text=path;editor_file.queue_free())
-		editor_file.visible = true
-		add_child(editor_file)
-		return
-		
-	var file_dialog : FileDialog = FileDialog.new()
-	file_dialog.title = "Select Chart's Events"
-	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-	file_dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-	file_dialog.filters = ["*" + current.get_events_extension()]
-	file_dialog.file_selected.connect(func(path:String)->void:events_line_edit.text=path;file_dialog.queue_free())
-	file_dialog.visible = true
-	add_child(file_dialog)
-		
-func output_file_dialog_pressed():
-	var current : Importer = importers[index]
-	if Engine.is_editor_hint():
-		var editor_file : EditorFileDialog = EditorFileDialog.new()
-		editor_file.title = "Select Output Folder"
-		editor_file.file_mode = EditorFileDialog.FILE_MODE_OPEN_DIR
-		editor_file.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-		editor_file.dir_selected.connect(func(path:String)->void:output_line_edit.text=path;editor_file.queue_free())
-		editor_file.visible = true
-		add_child(editor_file)
-		return
-		
-	var file_dialog : FileDialog = FileDialog.new()
-	file_dialog.title = "Select Output Folder"
-	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_DIR
-	file_dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
-	file_dialog.dir_selected.connect(func(path:String)->void:output_line_edit.text=path;file_dialog.queue_free())
+	file_dialog.filters = extensions
+	
+	file_dialog.file_selected.connect(func(path:String)->void:on_single_chosen.call(path);file_dialog.queue_free())
+	file_dialog.files_selected.connect(func(paths:PackedStringArray)->void:on_multiple_chosen.call(paths);file_dialog.queue_free())
+	file_dialog.dir_selected.connect(func(path:String)->void:on_single_chosen.call(path);file_dialog.queue_free())
+	
 	file_dialog.visible = true
 	add_child(file_dialog)
