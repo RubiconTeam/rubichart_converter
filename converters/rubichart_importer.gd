436039947e842cff15b07e6cf8ecc1600f48a6c3
Binpuki <30415792+Binpuki@users.noreply.github.com>
2024-12-25 03:08:44 -0600

    RubiChart option

---
+++ converters/rubichart_importer.gd
@@ -0,0 +1,20 @@
+@tool
+
+extends "res://addons/rubichart_converter/importer.gd"
+
+func get_name() -> String:
+	return "RubiChart Binary"
+	
+func get_extension() -> String:
+	return ".rbc"
+	
+func convert_chart(_chart : FileAccess, _meta : FileAccess, _events : FileAccess) -> Dictionary:
+	var chart : RubiChart = RubiChart.new()
+	chart.LoadBytes(_chart.get_buffer(_chart.get_length()))
+	
+	var chart_path : String = _chart.get_path()
+	return {
+		"charts": {chart_path.substr(chart_path.rfind("/") + 1, chart_path.rfind(".")) : chart},
+		"events": EventMeta.new(),
+		"meta": SongMeta.new()
+	}
